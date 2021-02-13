65ff255867a0fa542147a8f2e25d320b54dd2408
Pål Bøckmann <plbockmann@gmail.com>
2021-02-13 17:07:06 +0100

    Rendering html

---
+++ net_ninja-tutorial_series/public/app.js
@@ -0,0 +1,22 @@
+import { Invoice } from './models/Invoice.js';
+import { Payment } from './models/Payment.js';
+import { ListTemplate } from './models/ListTemplate.js';
+const form = document.querySelector('.new-item-form');
+//inputs
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
+});
---
+++ net_ninja-tutorial_series/src/app.ts
@@ -0,0 +1,29 @@
+import { Invoice } from './models/Invoice.js'
+import { Payment } from './models/Payment.js'
+import { HasFormatter } from './interfaces/HasFormatter.js'
+import { ListTemplate } from './models/ListTemplate.js';
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+//inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    let doc: HasFormatter;
+    if (type.value === 'invoice'){
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+
+    list.render(doc, type.value, 'end');
+})
